6acb407d43be155d5ca34ff8ce33dc6d7e5a7014
Alex <helloalexsr@gmail.com>
2020-10-05 22:03:42 +0200

    cotrolers

--- js/main.js
+++ js/main.js
@@ -4,25 +4,50 @@ canvas.width = 600;
 canvas.height = 400;
 
 /// Firts try!
-let spacePressed = false;
+let arrowUp = false;
+let arrowLeft = false;
+let arrowRight = false;
 /*
 let angle = 0;
 let gravity = 0.5;
-let friction = 0; 
+let friction = 0; ArrowLeft
 */
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     spaceship.update();
-    spaceship.draw();
+    spaceship.drawBackground();
+    spaceship.drawSpaceShip();
+    spaceship.rightMove();    
+    spaceship.leftMove();
+
+    
     requestAnimationFrame(animate);
 }
 
 animate();
+
 window.addEventListener('keydown', function(e){
-    if (e.code === 'Space') spacePressed = true;
+    if (e.code === 'ArrowUp') arrowUp = true;
+    console.log(e.code);
 });
 
 window.addEventListener('keyup', function(e){
-    if (e.code === 'Space') spacePressed = false;
+    if (e.code === 'ArrowUp') arrowUp = false;
     
+});
+
+window.addEventListener('keydown', function(e){
+    if (e.code === 'ArrowLeft') arrowLeft = true; 
+});
+
+window.addEventListener('keyup', function(e){
+    if (e.code === 'ArrowLeft') arrowLeft = false;
+});
+
+window.addEventListener('keydown', function(e){
+    if (e.code === 'ArrowRight') arrowRight = true;
+});
+
+window.addEventListener('keyup', function(e){
+    if (e.code === 'ArrowRight') arrowRight = false;
 });
--- js/spaceship.js
+++ js/spaceship.js
@@ -1,55 +1,92 @@
-class Spaceship{
-    constructor(){
-        this.x = 150;
-        this.y = 200;
-        this.gravity = 0.05;
-        this.acceleration = 0
-        this.friction = 0.4;
-        this.vy = 1;
+class Spaceship {
+    constructor() {
+        // Spaceship Size
         this.width = 20;
         this.height = 20;
-       // this.weight = 1;
+        //Position
+        this.x = 300;
+        this.y = 200;
+        //Forces
+        this.gravity = 0.04;
+        this.acceleration = 0
+        this.friction = 0.66;
+        this.vy = 1;
+        //Left and rigth
+        this.vx = 0;
+        this.orizontalSpeed = 0;
+        this.xFriction = 0.1;
+        this.floorForce = 0;
     }
 
-    update(){
+    update() {
 
-        if (spacePressed) this.impulse();
-        if (!spacePressed) this.acceleration = 0
+        console.log(this.vy);
         
-        
-        if (this.y + this.height > canvas.height){
-            this.vy = -this.vy * this.friction;            
-        }else{
+        if (arrowUp) this.impulse();
+        if (!arrowUp) this.acceleration = 0;
+
+
+        if (this.y + this.height > canvas.height) {
+            this.vy = -this.vy * this.friction;
+            this.floorForce = 0.5;
+        } else {
             this.vy += this.gravity
+            this.floorForce = 0;
         }
         this.vy += this.acceleration
         this.y += this.vy;
-        //if(this.y + this.height > canvas.height) this.y = canvas.height - this.height
+
+        // floor friction
+        if (this.y + this.height > canvas.height) {
+            this.vx = -this.vx * this.xFriction;
+            
+        } 
+        // Orizontal force
+        this.vx += this.orizontalSpeed;
+        this.x += this.vx
 
     }
-        /*
-        if (this.y > canvas.height - this.height){ 
-            this.y = canvas.height - this.height;
-            this.vy -= 0;
-        }else{
-            this.vy += this.weight;
-            this.vy *= this.gravity;
-            this.y += this.vy;
-        }
-        if (spacePressed) this.impulse();
-    }
-    */
-    draw(){
-        ctx.fillStyle = 'red';
+
+    drawSpaceShip() {
+        ctx.fillStyle = 'white';
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 
-    impulse(){
-       // this.y -= 3;
-        //this.y = canvas.height - 50
-        this.acceleration = -0.1
-       
+    drawBackground() {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, 600, 400)
     }
+
+    impulse() {
+        this.acceleration = -0.07;
+    }
+
+    leftMove() {
+       
+        console.log(this.orizontalSpeed)
+        if (arrowLeft) {
+            ctx.fillStyle = 'red';
+            ctx.fillRect(this.x + this.width, this.y, 10, 10)
+            this.orizontalSpeed = -0.1 - this.floorForce;
+        } else if (!arrowRight) {
+            ctx.fillStyle = 'black';
+            ctx.clearRect(this.x + this.width, this.y, 10, 10)
+            this.orizontalSpeed = 0;
+        }
+    }
+
+    rightMove() {
+        console.log(this.orizontalSpeed);
+        if (arrowRight) {
+            ctx.fillStyle = 'red';
+            ctx.fillRect(this.x -10, this.y, 10, 10)
+            this.orizontalSpeed = 0.1 + this.floorForce;
+        } else if (!arrowLeft) {
+            ctx.clearRect(this.x -10, this.y, 10, 10)
+            this.orizontalSpeed = 0;
+        }
+    }
+
 }
 
 const spaceship = new Spaceship();
